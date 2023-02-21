9ce553154ea1d2257df9a655d35dcf4ebf8101af
msancheezzz <marcoschezchiz@gmail.com>
2023-02-21 18:14:48 +0100

    pruebas bbdd

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ try:
     conexion.execute("""create table articulos (
                               codigo integer primary key autoincrement,
                               descripcion text,
-                              precio real
+                              precio integer
                         )""")
     print("se creo la tabla articulos")                        
 except sqlite3.OperationalError:
-    print("La tabla articulos ya existe")                    
+    print("La tabla articulos ya existe")
+
+conexion.execute("insert into articulos(descripcion, precio) values(?,?)", ("tetongas", 2))
+conexion.commit()
+
+cursor=conexion.execute("select codigo,descripcion,precio from articulos")
+for fila in cursor:
+    print(fila)
+
+
 conexion.close()
