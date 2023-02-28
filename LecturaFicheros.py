3041f143e3f56216c495adbc6002450095c59bd1
msancheezzz <marcoschezchiz@gmail.com>
2023-02-28 17:56:49 +0100

    Insertar

--- LecturaFicheros.py
+++ LecturaFicheros.py
@@ -1,3 +1,4 @@
 import pandas as pd
 
 data = pd.read_csv("Data/alerts.csv")
+print(data)
--- main.py
+++ main.py
@@ -4,17 +4,24 @@ conexion=sqlite3.connect("bd1.db")
 try:
     conexion.execute("""create table articulos (
                               codigo integer primary key autoincrement,
-                              descripcion text,
-                              precio integer
+                              fecha text,
+                              sid integer,
+                              msg text,
+                              clasification text, 
+                              prioridad integer,
+                              protocolo text,
+                              origen text, 
+                              destino text, 
+                              puerto integer
                         )""")
-    print("se creo la tabla articulos")                        
+    print("se creo la tabla articulos")
 except sqlite3.OperationalError:
     print("La tabla articulos ya existe")
 
-conexion.execute("insert into articulos(descripcion, precio) values(?,?)", ("tetongas", 2))
+conexion.execute("insert into articulos(fecha, sid, msg, clasification, prioridad, protocolo, origen, destino, puerto) values(?,?, ?, ?, ?, ?, ?, ?, ?)", ("2022-07-03 00:19:55",2402000,"ET DROP Dshield Block Listed Source group 1","Misc Attack",2,"UDP","146.88.240.0","172.19.0.0",1194))
 conexion.commit()
 
-cursor=conexion.execute("select codigo,descripcion,precio from articulos")
+cursor=conexion.execute("select codigo,msg from articulos")
 for fila in cursor:
     print(fila)
 
